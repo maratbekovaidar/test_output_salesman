f5a36905c5736e672d0df6a5bfe6331fe39d040b
Aidar Maratbekov <maratbekovaidar@gmail.com>
2024-05-24 09:19:48 +0500

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2345760639"
+    serviceWorkerVersion: "1810947596"
   }
 });
